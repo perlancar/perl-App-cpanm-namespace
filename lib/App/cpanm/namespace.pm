28c9a643892eeedb6c2bb8b87346b91d5aa9744e
perlancar (on pc-office) <perlancar@gmail.com>
2019-06-19 14:10:47 +0700

    Release v0.001

---
+++ lib/App/cpanm/namespace.pm
@@ -0,0 +1,14 @@
+package App::cpanm::namespace;
+
+# DATE
+# VERSION
+
+1;
+
+# ABSTRACT: Install all modules from certain namespace from CPAN
+
+=head1 DESCRIPTION
+
+See included script L<cpanm-namespace>.
+
+=cut
